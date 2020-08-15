64172311dc387b5e9a8aabac4bc589f9872b58cd
Pramesywara Jembar P <pramesywara_jp@apps.ipb.ac.id>
2020-08-15 10:43:25 +0700

    deploy pertama

---
+++ precache-manifest.b0a9feed88f82b64ec205c5209900315.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2062813fe1f929e12cd4b9bf151baca2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49a0e6ab4e7fd369579a",
+    "url": "/static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "b8212ece212271712c5b",
+    "url": "/static/css/main.0809585d.chunk.css"
+  },
+  {
+    "revision": "49a0e6ab4e7fd369579a",
+    "url": "/static/js/2.af58d5e4.chunk.js"
+  },
+  {
+    "revision": "72a28c3c11a5e18dc3a88d9511fe805a",
+    "url": "/static/js/2.af58d5e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8212ece212271712c5b",
+    "url": "/static/js/main.dc37968a.chunk.js"
+  },
+  {
+    "revision": "f2efdfc6827b5a8d7ee0",
+    "url": "/static/js/runtime-main.0433eb1b.js"
+  },
+  {
+    "revision": "c66f91c8d67c4287d78689ff04c03842",
+    "url": "/static/media/logo.c66f91c8.svg"
+  }
+]);
