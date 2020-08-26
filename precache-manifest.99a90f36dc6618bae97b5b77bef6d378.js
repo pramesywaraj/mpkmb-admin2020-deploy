cc817cfca7efee1f00e5b324e08e6f2accf12bcc
Pramesywara Jembar P <pramesywara_jp@apps.ipb.ac.id>
2020-08-26 11:18:41 +0700

    store feature

---
+++ precache-manifest.99a90f36dc6618bae97b5b77bef6d378.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6d23d3f899ca7a2534fa0faa6180daa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e17bd33945fd7c3b951",
+    "url": "/static/css/2.2e91a8db.chunk.css"
+  },
+  {
+    "revision": "ce394c7c37b4014a0633",
+    "url": "/static/css/main.61b787f2.chunk.css"
+  },
+  {
+    "revision": "4e17bd33945fd7c3b951",
+    "url": "/static/js/2.7ad0c4ad.chunk.js"
+  },
+  {
+    "revision": "60420cebb8610efe9a391400cc81dae3",
+    "url": "/static/js/2.7ad0c4ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce394c7c37b4014a0633",
+    "url": "/static/js/main.0f6f3047.chunk.js"
+  },
+  {
+    "revision": "f2efdfc6827b5a8d7ee0",
+    "url": "/static/js/runtime-main.0433eb1b.js"
+  },
+  {
+    "revision": "c66f91c8d67c4287d78689ff04c03842",
+    "url": "/static/media/logo.c66f91c8.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4893268d887a8aab47138de4f387caa5.js"
+  "/precache-manifest.99a90f36dc6618bae97b5b77bef6d378.js"
 );
 
 self.addEventListener('message', (event) => {
